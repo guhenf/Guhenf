338b91073047faaec74a463f6ad953e4781bb14d
Guhenf <guhenf@gmail.com>
2024-09-03 15:27:45 -0300

    keyframes em Header

--- src/html/header.tsx
+++ src/html/header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header>
-      <h1 className='text-lg'>Guhenf.</h1>
+      <h1 className='text-lg typing-effect'>Guhenf.</h1>
       <h2>Welcome to my Portfolio</h2>
     </header>
   )
