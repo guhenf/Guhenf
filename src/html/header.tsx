28e1c755a230ad7d0bb9910cfedeabd2950cc3ed
Guhenf <guhenf@gmail.com>
2024-09-04 13:38:09 -0300

    change font size

--- src/html/header.tsx
+++ src/html/header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header>
-      <h1 className='text-6xl typing-effect'>Guhenf.</h1>
+      <h1 className='text-4xl typing-effect'>Guhenf.</h1>
       <h2>Bem vindo ao meu Portfolio.</h2>
     </header>
   )
